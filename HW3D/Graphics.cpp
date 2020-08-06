be0a6ba3b57ad4f91c4416dafada088ea08e05b5
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-06 20:36:02 +0100

    Added dxerr files and graphics macros.

--- HW3D/Graphics.cpp
+++ HW3D/Graphics.cpp
@@ -1,6 +1,11 @@
 #include "Graphics.h"
+#include "dxerr.h"
+#include <sstream>
 
 #pragma comment(lib, "d3d11.lib")
+
+#define GFX_THROW_FAILED( hrcall ) if( FAILED( hr == hrcall ) ) throw Graphics::HrException( __LINE__, __FILE__, hr )
+#define GFX_DEVICE_REMOVED_EXCEPT( hr ) Graphics::DeviceRemovedException( __LINE__, __FILE__, ( hr ) );
 
 Graphics::Graphics( HWND hWnd )
 {
@@ -21,8 +26,10 @@ Graphics::Graphics( HWND hWnd )
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	sd.Flags = 0;
 
+	HRESULT hr;
+
 	// create device, front/back buffers, swap chain and rendering context
-	D3D11CreateDeviceAndSwapChain(
+	GFX_THROW_FAILED(D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
@@ -35,31 +42,11 @@ Graphics::Graphics( HWND hWnd )
 		&pDevice,
 		nullptr,
 		&pContext
-	);
+	));
 
 	// gain access to back buffer (sub-resource)
 	ID3D11Resource* pBackBuffer = nullptr;
-	pSwap->GetBuffer( 0, __uuidof( ID3D11Resource ), reinterpret_cast<void**>( &pBackBuffer ) );
-	pDevice->CreateRenderTargetView( pBackBuffer, nullptr, &pTarget );
+	GFX_THROW_FAILED( pSwap->GetBuffer( 0, __uuidof( ID3D11Resource ), reinterpret_cast<void**>( &pBackBuffer ) ) );
+	GFX_THROW_FAILED( pDevice->CreateRenderTargetView( pBackBuffer, nullptr, &pTarget ) ) ;
 	pBackBuffer->Release();
-}
-
-Graphics::~Graphics()
-{
-	if ( pContext != nullptr )
-		pContext->Release();
-
-	if ( pSwap != nullptr )
-		pSwap->Release();
-
-	if ( pDevice != nullptr )
-		pDevice->Release();
-
-	if ( pTarget != nullptr )
-		pTarget->Release();
-}
-
-void Graphics::EndFrame()
-{
-	pSwap->Present( 1u, 0u );
 }
--- HW3D/Graphics.h
+++ HW3D/Graphics.h
@@ -1,9 +1,33 @@
 #pragma once
 #include "WindowsInclude.h"
+#include "Exception.h"
 #include <d3d11.h>
 
 class Graphics
 {
+public:
+	class GfxException : public Exception
+	{
+		using Exception::Exception;
+	};
+	class HrException : public GfxException
+	{
+	public:
+		HrException( int line, const char* file, HRESULT hr ) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+	private:
+		HRESULT hr;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	};
 public:
 	Graphics( HWND hWnd );
 	Graphics( const Graphics&  ) = delete;
