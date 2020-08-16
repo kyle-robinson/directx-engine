42d7e264cdeb9540ec56be70f75b9e7188a4bdcd
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-16 19:14:18 +0100

    Added assimp source and header files for model loading.

---
+++ HW3D/AssimpObject.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "PrimitiveObject.h"
+#include "ConstantBuffers.h"
+
+class AssimpObject : public PrimitiveObject<AssimpObject>
+{
+public:
+	AssimpObject( Graphics& gfx, std::mt19937 rng,
+		std::uniform_real_distribution<float>& adist,
+		std::uniform_real_distribution<float>& ddist,
+		std::uniform_real_distribution<float>& odist,
+		std::uniform_real_distribution<float>& rdist,
+		DirectX::XMFLOAT3 material, float scale );
+};
