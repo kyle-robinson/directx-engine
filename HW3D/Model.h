dec5e6fc1d2d2d4c187ac42a979d54d8f7074516
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-17 22:08:02 +0100

    Added model class to header file.

--- HW3D/Model.h
+++ HW3D/Model.h
@@ -77,5 +77,104 @@ private:
 
 class Model
 {
+public:
+	Model( Graphics& gfx, const std::string fileName )
+	{
+		Assimp::Importer importer;
+		const auto pScene = importer.ReadFile(
+			fileName.c_str(),
+			aiProcess_Triangulate | aiProcess_JoinIdenticalVertices
+		);
 
+		for ( size_t i = 0; i < pScene->mNumMeshes; i++ )
+		{
+			meshPtrs.push_back( ParseMesh( gfx, *pScene->mMeshes[i] ) );
+		}
+
+		pRoot = ParseNode( *pScene->mRootNode );
+	}
+	static std::unique_ptr<Mesh> ParseMesh( Graphics& gfx, const aiMesh& mesh )
+	{
+		using VertexMeta::VertexLayout;
+
+		VertexMeta::VertexBuffer vbuf(
+			std::move(
+				VertexLayout{}
+				.Append( VertexLayout::Position3D )
+				.Append( VertexLayout::Normal )
+			)
+		);
+
+		for ( unsigned int i = 0; i < mesh.mNumVertices; i++ )
+		{
+			vbuf.EmplaceBack(
+				*reinterpret_cast<DirectX::XMFLOAT3*>( &mesh.mVertices[i] ),
+				*reinterpret_cast<DirectX::XMFLOAT3*>( &mesh.mNormals[i] )
+			);
+		}
+
+		std::vector<unsigned short> indices;
+		indices.reserve( mesh.mNumFaces * 3 );
+		for ( unsigned int i = 0; i < mesh.mNumFaces; i++ )
+		{
+			const auto& face = mesh.mFaces[i];
+			assert( face.mNumIndices == 3 );
+			indices.push_back( face.mIndices[0] );
+			indices.push_back( face.mIndices[1] );
+			indices.push_back( face.mIndices[2] );
+		}
+
+		std::vector<std::unique_ptr<Bindable>> bindablePtrs;
+		bindablePtrs.push_back( std::make_unique<VertexBuffer>( gfx, vbuf ) );
+		bindablePtrs.push_back( std::make_unique<IndexBuffer>( gfx, indices ) );
+
+		auto pvs = std::make_unique<VertexShader>( gfx, L"PhongVS.cso" );
+		auto pvsbc = pvs->GetByteCode();
+		bindablePtrs.push_back( std::move( pvs ) );
+		bindablePtrs.push_back( std::make_unique<PixelShader>( gfx, L"PhongPS.cso" ) );
+
+		bindablePtrs.push_back( std::make_unique<InputLayout>( gfx, vbuf.GetLayout().GetD3DLayout(), pvsbc ) );
+
+		struct PSMaterialCount
+		{
+			DirectX::XMFLOAT3 color = { 0.6f, 0.6f, 0.8f };
+			float speuclarIntensity = 0.6f;
+			float specularPower = 30.0f;
+			float padding[3];
+		} pmc;
+		bindablePtrs.push_back( std::make_unique<PixelConstantBuffer<PSMaterialCount>>( gfx, pmc, 1u ) );
+
+		return std::make_unique<Mesh>( gfx, std::move( bindablePtrs ) );
+	}
+	std::unique_ptr<Node> ParseNode( const aiNode& node )
+	{
+		const auto transform = DirectX::XMMatrixTranspose(
+			DirectX::XMLoadFloat4x4(
+				reinterpret_cast<const DirectX::XMFLOAT4X4*>( &node.mTransformation )
+			)
+		);
+
+		std::vector<Mesh*> curMeshPtrs;
+		curMeshPtrs.reserve( node.mNumMeshes );
+		for ( size_t i = 0; i < node.mNumMeshes; i++ )
+		{
+			const auto meshIdx = node.mMeshes[i];
+			curMeshPtrs.push_back( meshPtrs.at( meshIdx ).get() );
+		}
+
+		auto pNode = std::make_unique<Node>( std::move( curMeshPtrs ), transform );
+		for ( size_t i = 0; i < node.mNumChildren; i++ )
+		{
+			pNode->AddChild( ParseNode( *node.mChildren[i] ) );
+		}
+
+		return pNode;
+	}
+	void Draw( Graphics& gfx ) const
+	{
+		pRoot->Draw( gfx, DirectX::XMMatrixIdentity() );
+	}
+private:
+	std::unique_ptr<Node> pRoot;
+	std::vector<std::unique_ptr<Mesh>> meshPtrs;
 };
