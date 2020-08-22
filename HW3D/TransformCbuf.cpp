4e4e5871a550b7636114c709ed713ff7bab3db48
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-22 23:12:46 +0100

    Add extra bindable for dual vertex and pixel transform binding.

--- HW3D/TransformCbuf.cpp
+++ HW3D/TransformCbuf.cpp
@@ -10,14 +10,23 @@ namespace Bind
 
 	void TransformCbuf::Bind( Graphics& gfx ) noexcept
 	{
+		UpdateBind( gfx, GetTransforms( gfx ) );
+	}
+
+	void TransformCbuf::UpdateBind( Graphics& gfx, const Transforms& tf ) noexcept
+	{
+		pVcbuf->Update(gfx, tf);
+		pVcbuf->Bind(gfx);
+	}
+
+	TransformCbuf::Transforms TransformCbuf::GetTransforms( Graphics& gfx ) noexcept
+	{
 		const auto modelView = parent.GetTransformXM() * gfx.GetCamera();
-		const Transforms tf =
+		return
 		{
 			DirectX::XMMatrixTranspose( modelView ),
 			DirectX::XMMatrixTranspose( modelView * gfx.GetProjection() )
 		};
-		pVcbuf->Update( gfx, tf );
-		pVcbuf->Bind( gfx );
 	}
 
 	std::unique_ptr<VertexConstantBuffer<TransformCbuf::Transforms>> TransformCbuf::pVcbuf;
--- HW3D/TransformCbuf.h
+++ HW3D/TransformCbuf.h
@@ -9,12 +9,15 @@ namespace Bind
 	public:
 		TransformCbuf( Graphics& gfx, const Drawable& parent, UINT slot = 0u );
 		void Bind( Graphics& gfx ) noexcept override;
-	private:
+	protected:
 		struct Transforms
 		{
 			DirectX::XMMATRIX modelViewProj;
-			DirectX::XMMATRIX model;
+			DirectX::XMMATRIX modelView;
 		};
+		void UpdateBind( Graphics& gfx, const Transforms& tf ) noexcept;
+		Transforms GetTransforms( Graphics& gfx ) noexcept;
+	private:
 		static std::unique_ptr<VertexConstantBuffer<Transforms>> pVcbuf;
 		const Drawable& parent;
 	};
