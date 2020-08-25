037932cb1a31913970fdd0bc487044580c985679
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-25 22:18:01 +0100

    Added texture mipmapping.

--- HW3D/Texture.cpp
+++ HW3D/Texture.cpp
@@ -15,32 +15,40 @@ namespace Bind
 
 		// create texture resource
 		D3D11_TEXTURE2D_DESC textureDesc = { 0 };
-		textureDesc.Width = (UINT)s.GetWidth();
-		textureDesc.Height = (UINT)s.GetHeight();
-		textureDesc.MipLevels = 1u;
+		textureDesc.Width = s.GetWidth();
+		textureDesc.Height = s.GetHeight();
+		textureDesc.MipLevels = 0u;
 		textureDesc.ArraySize = 1u;
 		textureDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 		textureDesc.SampleDesc.Count = 1u;
 		textureDesc.SampleDesc.Quality = 0u;
 		textureDesc.Usage = D3D11_USAGE_DEFAULT;
-		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 		textureDesc.CPUAccessFlags = 0u;
-		textureDesc.MiscFlags = 0u;
+		textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 
 		D3D11_SUBRESOURCE_DATA sd = { 0 };
 		sd.pSysMem = s.GetBufferPtr();
-		sd.SysMemPitch = (UINT)s.GetWidth() * sizeof( Surface::Color );
+		sd.SysMemPitch = s.GetWidth() * sizeof( Surface::Color );
 
 		Microsoft::WRL::ComPtr<ID3D11Texture2D> pTexture;
-		GFX_THROW_INFO( GetDevice( gfx )->CreateTexture2D( &textureDesc, &sd, &pTexture ) );
+		GFX_THROW_INFO( GetDevice( gfx )->CreateTexture2D( &textureDesc, nullptr, &pTexture ) );
+
+		// write image data to top mip level
+		GFX_THROW_INFO_ONLY( GetContext( gfx )->UpdateSubresource(
+			pTexture.Get(), 0u, nullptr, s.GetBufferPtrConst(), s.GetWidth() * sizeof( Surface::Color ), 0u
+		) );
 
 		// create the resource view on the texture
 		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 		srvDesc.Format = textureDesc.Format;
 		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-		srvDesc.Texture2D.MostDetailedMip = 0u;
-		srvDesc.Texture2D.MipLevels = 1u;
+		srvDesc.Texture2D.MostDetailedMip = 0;
+		srvDesc.Texture2D.MipLevels = -1;
 		GFX_THROW_INFO( GetDevice( gfx )->CreateShaderResourceView( pTexture.Get(), &srvDesc, &pTextureView ) );
+
+		// generate mip chain
+		GFX_THROW_INFO_ONLY( GetContext( gfx )->GenerateMips( pTextureView.Get() ) );
 	}
 
 	void Texture::Bind( Graphics& gfx ) noexcept
