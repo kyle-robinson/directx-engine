307f9495f371d225bfc5e8a72c10df9ac362b59f
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-08-28 19:43:43 +0100

    Completed layout codex files.

--- HW3D/LayoutCodex.cpp
+++ HW3D/LayoutCodex.cpp
@@ -1,1 +1,29 @@
 #include "LayoutCodex.h"
+
+namespace DCB
+{
+	DCB::CompleteLayout LayoutCodex::Resolve( DCB::RawLayout&& layout ) noexcept(!IS_DEBUG)
+	{
+		auto sig = layout.GetSignature();
+		auto& map = Get_().map;
+		const auto i = map.find( sig );
+
+		// identical layout already exists
+		if ( i != map.end() )
+		{
+			layout.ClearRoot();
+			return { i->second };
+		}
+
+		// otherwise add layout root element to map
+		auto result = map.insert( { std::move( sig ), layout.DeliverRoot() } );
+		// return layout with extra ref to root
+		return { result.first->second };
+	}
+
+	LayoutCodex& LayoutCodex::Get_() noexcept
+	{
+		static LayoutCodex codex;
+		return codex;
+	}
+}
--- HW3D/LayoutCodex.h
+++ HW3D/LayoutCodex.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "DynamicConstant.h"
+#include <unordered_map>
+#include <string>
+#include <memory>
 
-class LayoutCodex
+namespace DCB
 {
-
-};
+	class LayoutCodex
+	{
+	public:
+		static DCB::CompleteLayout Resolve( DCB::RawLayout&& layout ) noexcept(!IS_DEBUG);
+	private:
+		static LayoutCodex& Get_() noexcept;
+		std::unordered_map<std::string, std::shared_ptr<DCB::LayoutElement>> map;
+	};
+}
