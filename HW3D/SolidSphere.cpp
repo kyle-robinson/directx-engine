3258570825bf95d6b4cdbd5b5300d3b0596d4e82
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-09-15 00:36:44 +0100

    Blur outline technique added to light sphere.

--- HW3D/SolidSphere.cpp
+++ HW3D/SolidSphere.cpp
@@ -20,26 +20,56 @@ SolidSphere::SolidSphere( Graphics& gfx, float radius )
 	pIndices = IndexBuffer::Resolve( gfx, geometryTag, model.indices );
 	pTopology = Topology::Resolve( gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
 
+	auto tcb = std::make_shared<TransformCbuf>( gfx );
 	{
-		Technique solid{ Channel::main };
-		Step initial( "lambertian" );
+		Technique solid( "Shading", Channel::main );
+		{
+			Step initial( "lambertian" );
 
-		auto pvs = VertexShader::Resolve( gfx, "SolidVS.cso" );
-		initial.AddBindable( InputLayout::Resolve( gfx, model.vertices.GetLayout(), *pvs ) );
-		initial.AddBindable( std::move( pvs ) );
-		initial.AddBindable( PixelShader::Resolve( gfx, "SolidPS.cso" ) );
+			auto pvs = VertexShader::Resolve( gfx, "SolidVS.cso" );
+			initial.AddBindable( InputLayout::Resolve( gfx, model.vertices.GetLayout(), *pvs ) );
+			initial.AddBindable( std::move( pvs ) );
+			initial.AddBindable( PixelShader::Resolve( gfx, "SolidPS.cso" ) );
 
-		Dcb::RawLayout layout;
-		layout.Add<Dcb::Float3>( "color" );
-		auto buffer = Dcb::Buffer( std::move( layout ) );
-		buffer["color"] = DirectX::XMFLOAT3{ 1.0f, 1.0f, 1.0f };
+			Dcb::RawLayout layout;
+			layout.Add<Dcb::Float3>( "color" );
+			auto buffer = Dcb::Buffer( std::move( layout ) );
+			buffer["color"] = DirectX::XMFLOAT3{ 1.0f, 1.0f, 1.0f };
 
-		initial.AddBindable( std::make_shared<CachingPixelConstantBufferEx>( gfx, buffer, 1u ) );
-		initial.AddBindable( std::make_shared<TransformCbuf>( gfx ) );
-		initial.AddBindable( Rasterizer::Resolve( gfx, false ) );
+			initial.AddBindable( std::make_shared<CachingPixelConstantBufferEx>( gfx, buffer, 1u ) );
+			initial.AddBindable( tcb );
+			initial.AddBindable( Rasterizer::Resolve( gfx, false ) );
 
-		solid.AddStep( std::move( initial ) );
+			solid.AddStep( std::move( initial ) );
+		}
 		AddTechnique( std::move( solid ) );
+	}
+
+	{
+		Technique outline( "Outline", Channel::main );
+		{
+			Step mask( "outlineMask" );
+
+			mask.AddBindable( InputLayout::Resolve( gfx, model.vertices.GetLayout(), *VertexShader::Resolve( gfx, "SolidVS.cso" ) ) );
+			mask.AddBindable( std::move( tcb ) );
+
+			outline.AddStep( std::move( mask ) );
+		}
+		{
+			Step draw( "outlineDraw" );
+
+			Dcb::RawLayout layout;
+			layout.Add<Dcb::Float4>( "blurColor" );
+			auto buf = Dcb::Buffer( std::move( layout ) );
+			buf["blurColor"] = DirectX::XMFLOAT4{ 1.0f, 0.4f, 0.4f, 1.0f };
+			draw.AddBindable( std::make_shared<Bind::CachingPixelConstantBufferEx>( gfx, buf, 1u ) );
+
+			draw.AddBindable( InputLayout::Resolve( gfx, model.vertices.GetLayout(), *VertexShader::Resolve( gfx, "SolidVS.cso" ) ) );
+			draw.AddBindable( std::make_shared<TransformCbuf>( gfx ) );
+
+			outline.AddStep( std::move( draw ) );
+		}
+		AddTechnique( std::move( outline ) );
 	}
 }
 
@@ -66,6 +96,8 @@ void SolidSphere::SpawnControls() noexcept
 
 			if ( auto v = buf["color"]; v.Exists() )
 				linkCheck( ImGui::ColorEdit3( tag( "Sphere" ), reinterpret_cast<float*>( &static_cast<DirectX::XMFLOAT3&>( v ) ) ) );
+			if ( auto v = buf["blurColor"]; v.Exists() )
+				linkCheck( ImGui::ColorEdit4( tag( "Outline" ), reinterpret_cast<float*>( &static_cast<DirectX::XMFLOAT4&>( v ) ) ) );
 
 			return bufferSet;
 		}
